30f4d74f1053b6d9940f5f6baaaab3cae72135a7
charlesmjohnson0 <89574743+charlesmjohnson0@users.noreply.github.com>
2021-12-29 16:59:44 +0800

    dd

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
-  Win32Window::Point origin(-10, 10);
+  Win32Window::Point origin(60, 10);
   Win32Window::Size size(640, 960);
   if (!window.CreateAndShow(L"Fastway", origin, size))
   {
